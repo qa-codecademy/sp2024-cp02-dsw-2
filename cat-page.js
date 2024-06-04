4a3b2887392dfec49e687a823264f85f3d2e57ad
Tea <150371708+sarcasticcatx@users.noreply.github.com>
2024-06-05 00:59:13 +0200

    Update cat-page.js

--- cat-page.js
+++ cat-page.js
@@ -1,33 +1,44 @@
 const productsContainerEl = document.querySelector(".products-container");
+const loadButton = document.querySelector(".loadingProducts");
 
-const PRODUCTS_URL = "cat.json";
+const localProductsStorage = "./src/assets/data/products.json";
 
-function fetchProducts() {
-  fetch(PRODUCTS_URL)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      renderProductsList(productsContainerEl, data);
-    });
+async function fetchProductsAndPutInLocalStorage() {
+  try {
+    const res = await fetch(localProductsStorage);
+    const data = await res.json();
+    localStorage.setItem("localProducts", JSON.stringify(data));
+    renderProductsList(productsContainerEl, data);
+  } catch (error) {
+    console.error("failing to fetch ur products", error);
+  }
 }
 
 function renderProductsList(containerEl, productsData) {
   let productsHTML = "";
 
-  for (let product of productsData) {
+  for (let products of productsData) {
     productsHTML += `
     <div class="product-card">
-      <h3>${product.id}. ${product.name}</h3>
-      <h4>Price: $${product.price}</h4>
-      <img src="${product.image}" alt="" />
-      <p>${product.description}</p>
+      <h3>${products.id}. ${products.name}</h3>
+      <img src="${products.image}" alt="" />
+      <h4>Price: $${products.price}</h4>
+      <h4>Category: ${products.category}</h4>
     </div>
     `;
   }
 
   containerEl.innerHTML = productsHTML;
+
+  loadButton.addEventListener("click", () => fetchProductsAndPutInLocalStorage);
+
+  if (localStorage.getItem("localProducts")) {
+    const productsInStorage = JSON.parse(localStorage.getItem("localProducts"));
+    renderProductsList(productsContainerEl, productsInStorage);
+  } else {
+    fetchProductsAndPutInLocalStorage();
+  }
 }
 
-fetchProducts();
+console.log(localProductsStorage);
+console.log(loadButton);
