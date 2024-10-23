d2cbfed3ca6eb68da03c1531f32d61c57d04c2b8
Biljana <biljanapejoska985@gmail.com>
2024-10-23 23:36:55 +0200

    add user registration functionalities and API for user reg and auth

--- src/models/login/login.js
+++ src/models/login/login.js
@@ -1,33 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.getElementById('login-form');
-    const loginMessage = document.getElementById('login-message');
-    
+    const loginForm = document.getElementById("login-form");
+    const registrationForm = document.getElementById("registrationForm");
+   
 
-    // Add submit event listener to the login form
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault();     
-        
+   // Add submit event listener to the login form
+    loginForm.addEventListener("submit", async function(event) {
+        event.preventDefault();
+        const email = document.getElementById("login-email").value;
+        const password = document.getElementById("login-password").value;
 
-         // Get username and password from the form
-        const username = this.username.value;
-        const password = this.password.value;
-
-        authenticateUser(username, password);
+        console.log("Login attempt with:", email, password); 
+        await onLogin(email, password); 
     });
 
-    // Function to authenticate user
-    function authenticateUser(username, password) {
-        const savedUserString = localStorage.getItem('user');
-        const savedUser = savedUserString ? JSON.parse(savedUserString) : null;
-        console.log('Saved user from localStorage:', savedUser);
-    
-        if (savedUser && (username === savedUser.email || username === savedUser.username) && password === savedUser.password) {
-            localStorage.setItem('isLoggedIn', true);
-            loginMessage.textContent = 'Пријава успешна!';
-           
-            window.location.href = '/';
-        } else {
-          loginMessage.textContent = "Invalid username or password"
+    // Submit for registration 
+    registrationForm.addEventListener("submit", async function(event) {
+        event.preventDefault();
+        const email = document.getElementById("reg-email").value;
+        const password = document.getElementById("reg-password").value;
+        const confirmPassword = document.getElementById("confirm-password").value;
+        const username = document.getElementById('reg-username').value;
+
+        if (password !== confirmPassword) {
+            document.getElementById('registration-message').textContent = "Passwords don't match";
+            return;
         }
-    }
+
+        console.log("Registration attempt with:", email, password); 
+        await onRegister(email, password, username); 
+    });
 });
+
+ // Function to authenticate user
+async function authenticateUser(email, password) {
+    try {
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log('Successful login:', data);
+           
+        } else {
+            const errorData = await response.json();
+            console.error('error:', errorData.message);
+        }
+    } catch (error) {
+        console.error('error', error);
+    }
+}
+
+
+
+
+
